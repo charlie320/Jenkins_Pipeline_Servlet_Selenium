636de4907c6c543a907681230dc3c42b54b54642
Charlie de la Rosa <jcdelarosa@verizon.net>
2020-05-28 14:08:53 -0500

    Added dummy unit tests

---
+++ QEModule_Jenkins_Pipeline_Servlet_Selenium/src/test/java/com/perscholas/jenkins_pipeline/unit_tests/UnitTest01.java
@@ -0,0 +1,14 @@
+package com.perscholas.jenkins_pipeline.unit_tests;
+
+import org.testng.annotations.Test;
+
+public class UnitTest01 {
+	@Test
+	public void unitTest01Method01() {
+		System.out.println("unitTest02Method01");
+	}
+	@Test
+	public void unitTest01Method02() {
+		System.out.println("unitTest02Method02");
+	}
+}
---
+++ QEModule_Jenkins_Pipeline_Servlet_Selenium/src/test/java/com/perscholas/jenkins_pipeline/unit_tests/UnitTest02.java
@@ -0,0 +1,14 @@
+package com.perscholas.jenkins_pipeline.unit_tests;
+
+import org.testng.annotations.Test;
+
+public class UnitTest02 {
+	@Test
+	public void unitTest02Method01() {
+		System.out.println("unitTest02Method01");
+	}
+	@Test
+	public void unitTest02Method02() {
+		System.out.println("unitTest02Method02");
+	}
+}
