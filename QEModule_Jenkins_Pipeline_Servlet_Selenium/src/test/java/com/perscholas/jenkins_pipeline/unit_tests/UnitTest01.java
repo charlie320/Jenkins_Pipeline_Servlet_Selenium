1498a53d6c72abedb028e4c0a3e01684fc819341
Charlie de la Rosa <jcdelarosa@verizon.net>
2021-02-23 14:05:57 -0600

    Renamed unit tests back to original

--- QEModule_Jenkins_Pipeline_Servlet_Selenium/src/test/java/com/perscholas/jenkins_pipeline/unit_tests/UnitTest01.java
+++ QEModule_Jenkins_Pipeline_Servlet_Selenium/src/test/java/com/perscholas/jenkins_pipeline/unit_tests/UnitTest01.java
@@ -2,13 +2,13 @@ package com.perscholas.jenkins_pipeline.unit_tests;
 
 import org.testng.annotations.Test;
 
-public class Unit02Test {
+public class UnitTest01 {
 	@Test
-	public void unitTest02Method01() {
+	public void unitTest01Method01() {
 		System.out.println("unitTest02Method01");
 	}
 	@Test
-	public void unitTest02Method02() {
+	public void unitTest01Method02() {
 		System.out.println("unitTest02Method02");
 	}
 }
--- QEModule_Jenkins_Pipeline_Servlet_Selenium/src/test/java/com/perscholas/jenkins_pipeline/unit_tests/UnitTest02.java
+++ QEModule_Jenkins_Pipeline_Servlet_Selenium/src/test/java/com/perscholas/jenkins_pipeline/unit_tests/UnitTest02.java
@@ -2,13 +2,13 @@ package com.perscholas.jenkins_pipeline.unit_tests;
 
 import org.testng.annotations.Test;
 
-public class Unit01Test {
+public class UnitTest02 {
 	@Test
-	public void unitTest01Method01() {
+	public void unitTest02Method01() {
 		System.out.println("unitTest02Method01");
 	}
 	@Test
-	public void unitTest01Method02() {
+	public void unitTest02Method02() {
 		System.out.println("unitTest02Method02");
 	}
 }
